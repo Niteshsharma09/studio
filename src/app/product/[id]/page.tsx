f657bfe67a85c5bbb54fa11ec04f2b36139a4b61
Nitesh Sharma <pandiitniitesh123@gmail.com>
2025-11-27 17:48:44 +0000

    add dark colour in add to cart and buy now button

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -174,12 +174,12 @@ export default function ProductDetailPage() {
                 className="w-20"
                 aria-label="Quantity"
               />
-              <Button size="lg" className="flex-1" onClick={handleAddToCart}>
+              <Button size="lg" className="flex-1 bg-gray-800 hover:bg-gray-900" onClick={handleAddToCart}>
                 <ShoppingBag className="mr-2 h-5 w-5" />
                 Add to Cart
               </Button>
             </div>
-            <Button size="lg" variant="secondary" className="w-full" onClick={handleBuyNow}>
+            <Button size="lg" variant="default" className="w-full bg-gray-800 hover:bg-gray-900" onClick={handleBuyNow}>
               <Zap className="mr-2 h-5 w-5" />
               Buy Now
             </Button>
