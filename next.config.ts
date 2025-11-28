bd17820ba34001ad7a2669e2f030cfd871480e64
Nitesh Sharma <pandiitniitesh123@gmail.com>
2025-11-28 17:54:57 +0000

    til now its is not shown

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,9 @@
 import type {NextConfig} from 'next';
+import path from 'path';
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -23,7 +28,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https://picsum.photos',
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
