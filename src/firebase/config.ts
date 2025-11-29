7f04ceeb96eb72ac3741457989943f38da6af49f
Nitesh Sharma <pandiitniitesh123@gmail.com>
2025-11-29 08:27:46 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,4 +2,11 @@
 // The Firebase configuration will be populated by the Firebase CLI.
 // See the Firebase documentation for more information:
 // https://firebase.google.com/docs/web/setup#add-sdks-initialize
-export const firebaseConfig = {};
+export const firebaseConfig = {
+  "projectId": "studio-5151940003-12092",
+  "appId": "1:787037860208:web:c997827a270f0746aa1fb5",
+  "apiKey": "AIzaSyAjc0o5jn5RLA2eABlTkzUxY8MiZQPvKdY",
+  "authDomain": "studio-5151940003-12092.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "787037860208"
+};
