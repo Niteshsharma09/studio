bcc9d13b69c39d5faa085a579eb0975e60a2825c
Nitesh Sharma <pandiitniitesh123@gmail.com>
2025-12-04 13:00:15 +0000

    Error: Firebase Error: storage/no-default-bucket - Firebase Storage: No

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -7,6 +7,7 @@ export const firebaseConfig = {
   "appId": "1:787037860208:web:c997827a270f0746aa1fb5",
   "apiKey": "AIzaSyAjc0o5jn5RLA2eABlTkzUxY8MiZQPvKdY",
   "authDomain": "studio-5151940003-12092.firebaseapp.com",
+  "storageBucket": "studio-5151940003-12092.appspot.com",
   "measurementId": "",
   "messagingSenderId": "787037860208"
 };
